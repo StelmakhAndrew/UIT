9efe0fa7e122c4b6226c21199743f4c5803583f0
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2019-04-20 20:28:29 +0300

    refactoring

--- src/main/java/project/com/Controllers/GreetingController.java
+++ src/main/java/project/com/Controllers/GreetingController.java
@@ -30,6 +30,10 @@ public class GreetingController {
     public String greetings(Model model,@RequestParam(name = "inn") String inn) {
         //  SELECT * FROM public.documentpackage WHERE inn = 'inn'
         List<DocumentPackage> documentPackageList = documentPackageService.findAllByInn(inn);
+
+        if (documentPackageList.size()==0)
+            return "redirect:/";
+
         model.addAttribute("inn",inn);
         model.addAttribute("package",documentPackageList);
         return "inn";
@@ -37,7 +41,7 @@ public class GreetingController {
 
 
     @RequestMapping(value = "/edit", method = RequestMethod.POST)
-    public String editData(Model model,@RequestParam(name = "inn") String inn,
+    public String editData(@RequestParam(name = "inn") String inn,
             @RequestParam(name = "name") String name,
             @RequestParam(name = "data") String data) {
         //SELECT * FROM public.documentpackage AS package WHERE package.inn = 'ds' LIMIT 1
